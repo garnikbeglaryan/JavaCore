af1f39b7c45711a1e54ef707b1fd853b77148f9d
Garnik <Garo_2000>
2021-10-27 00:37:34 +0530

    Initial commit

---
+++ src/HomeWork5/Example.java
@@ -0,0 +1,274 @@
+package HomeWork5;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//
+//        }
+//        System.out.println();
+//
+//
+//        System.out.println("\n orinak 2");
+//
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("  ");
+//
+//            }
+//            for (int k = 0; k <= i; k++) {
+//                System.out.print(" *");
+//            }
+//            System.out.println();
+//
+//        }
+//        System.out.println();
+//
+//
+//        System.out.println("\n orinak 3");
+//
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = i; j < 4; j++) {
+//                System.out.print(" *");
+//
+//            }
+//            System.out.println();
+//        }
+//
+//        System.out.println();
+//
+//
+//        System.out.println("\n orinak 4");
+//
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("  ");
+//
+//            }
+//
+//            for (int k = i; k < 4; k++) {
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        System.out.println();
+//
+//
+//        System.out.println("\norinak 5");
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("  ");
+//
+//            }
+//            for (int k = 0; k <= i; k++) {
+//                System.out.print("  * ");
+//
+//            }
+//            System.out.println();
+//
+//        }
+//        for (int i = 3; i <0; i++) {
+//            for (int j = 0; j <=i; j++) {
+//                System.out.print("  ");
+//            }
+//
+//            for (int k = 0; k <= i; k++) {
+//                System.out.print(" *");
+//            }
+//        }System.out.println();
+
+
+
+
+
+
+
+
+
+
+        //Տպեք մասիվի բոլոր էլեմենտները
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + " ");
+//
+//        }
+
+
+        //Տպեք մասիվի ամենամեծ թիվը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int max = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > max) ;
+//            max = array[i];
+//
+//        }
+//
+//        System.out.println("max = " + max);
+
+
+        //Տպեք մասիվի ամենափոքրը թիվը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int min = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < min)
+//            min = array[i];
+//
+//        }
+//        System.out.println("min =" + min);
+
+
+        //Տպեք մասիվի բոլոր զույգ էլեմենտները
+
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0)
+//                System.out.println(array[i]);
+//
+//        }
+
+
+        //Տպեք մասիվի բոլոր կենտ էլեմենտները
+
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0)
+//                System.out.println(array[i]);
+//        }
+
+
+
+
+        //Տպեք զույգերի քանակը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//                count++;
+//
+//            }
+//
+//        }
+//        System.out.println(count);
+
+
+        //Տպեք կենտերի քանակը
+//
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                count++;
+//
+//            }
+//        }
+//        System.out.println(count);
+
+
+        //Տպեք մասիվի բոլոր թվերի միջին թվաբանականը
+        //Տպեք մասիվի էլեմենտների գումարը։
+
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        double money = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            money += array[i];
+//
+//        }
+//        System.out.println(money);
+//        System.out.println(money / array.length);
+
+
+
+
+
+
+
+
+
+
+        //Պետք է գրել լոգիկա, որը կհաշվի թե քանի հատ կա c-փոփոխականից մեր մասիվի մեջ, ու տպել այդ քանակը։
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        int count = 0;
+//        for (int i = 0; i < bolola.length; i++) {
+//            char c = bolola[i];
+//            if (c == 'o') {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+
+
+
+
+
+        //Գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները։
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        System.out.println(bolola[bolola.length /2 -1]);
+//        System.out.println(bolola[bolola.length / 2]);
+
+
+
+
+
+
+
+        // Տպենք true եթե մեր մասիվը վերջանում է ly-ով, եթե ոչ՝ false
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        if (bolola[bolola.length - 2] == 'l' && bolola[bolola.length - 1] == 'y') {
+//            System.out.println(true);
+//
+//        } else
+//            System.out.println(false);
+
+
+
+
+
+
+        //Տպել true եթե տեքստը պարունակում է bob բառը
+
+
+//        char[] bolola = {'b', 'a', 'b', 'o', 'l', 'a'};
+//        for (int i = 0; i < bolola.length; i++) {
+//            if (bolola[i] == 'b' && bolola[i + 2] == 'b') {
+//                System.out.println(true);
+//                  break;
+//            }else
+//                System.out.println(false);
+//                  break;
+//       }
+
+
+
+
+
+
+    }
+}
